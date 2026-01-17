cd36c4705ebc674bf19e4652bf91e715005e76cb
Larry Wang <lawrence.wang@datadoghq.com>
2026-01-17 16:30:03 -0500

    complete day 12

---
+++ day12/benchmarks.md
@@ -0,0 +1,3 @@
+| Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
+|:---|---:|---:|---:|---:|
+| `./target/release/p1` | 1.4 ± 0.6 | 0.7 | 5.6 | 1.00 |
--- day12/src/bin/p1.rs
+++ day12/src/bin/p1.rs
@@ -1,35 +1,44 @@
 use std::fs;
 
-fn solve(puzzle: &str, pieces: &Vec<Vec<&str>>) -> u128 {
-    
+fn solve(puzzle: &str, piece_counts: &Vec<usize>) -> bool {
     let (board, counts) = puzzle.split_once(':').unwrap();
+    let counts: Vec<&str> = counts.split_ascii_whitespace().collect();
     let (nrows, ncols) = board.split_once('x').unwrap();
-    let counts: Vec<&str> = counts.split_whitespace().collect();
+    let nrows: usize = nrows.parse().unwrap();
+    let ncols: usize = ncols.parse().unwrap();
 
-    dbg!(nrows, ncols);
-    dbg!(counts);
+    let total_pieces: usize = piece_counts
+        .iter()
+        .enumerate()
+        .map(|(i, npiece)| npiece * (counts[i].parse::<usize>().unwrap()))
+        .sum();
 
-    0
-
+    return total_pieces <= nrows * ncols;
 }
 
-fn main(){
-
-    let contents = fs::read_to_string("test.txt").unwrap();
+fn main() {
+    let contents = fs::read_to_string("input.txt").unwrap();
     let mut sections = contents.split("\n\n");
     let mut pieces: Vec<Vec<&str>> = Vec::new();
+    let mut counts = Vec::new();
 
     // process the buttons
-    for _ in 0..=5{
+    for _ in 0..=5 {
         let button = &sections.next().unwrap()[3..];
+        let key_count = button.matches('#').count();
         let button: Vec<&str> = button.split('\n').collect();
         pieces.push(button);
+        counts.push(key_count);
     }
 
     let puzzles: Vec<&str> = sections.next().unwrap().split('\n').collect();
+    let mut answer = 0;
 
-    for puzzle in puzzles{
-        _ = solve(puzzle, &pieces);
+    for puzzle in puzzles {
+        if solve(puzzle, &counts) {
+            answer += 1;
+        }
     }
-    
+
+    dbg!(answer);
 }
